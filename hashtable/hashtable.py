114ee8755762e33ee1d206458d43a32a65aae5a5
josegocampo <jose.ocampo@mail.udp.cl>
2020-08-10 16:22:26 -0400

    finishing MVP for part 1 and 2

--- hashtable/hashtable.py
+++ hashtable/hashtable.py
@@ -6,6 +6,8 @@ class HashTableEntry:
         self.key = key
         self.value = value
         self.next = None
+    
+     
 
 
 # Hash table can't have fewer than this many slots
@@ -13,43 +15,40 @@ MIN_CAPACITY = 8
 
 
 class HashTable:
+    count = 0
     """
     A hash table that with `capacity` buckets
     that accepts string keys
-
     Implement this.
     """
 
-    def __init__(self, capacity):
-        # Your code here
-
+    def __init__(self, capacity, count=0):
+        self.capacity = [None] * capacity
+        self.count = count
+        
 
     def get_num_slots(self):
         """
         Return the length of the list you're using to hold the hash
         table data. (Not the number of items stored in the hash table,
         but the number of slots in the main list.)
-
         One of the tests relies on this.
-
         Implement this.
         """
-        # Your code here
+        return len(self.capacity)
 
 
     def get_load_factor(self):
         """
         Return the load factor for this hash table.
-
         Implement this.
         """
-        # Your code here
+        return len(self.capacity) / self.count
 
 
     def fnv1(self, key):
         """
         FNV-1 Hash, 64-bit
-
         Implement this, and/or DJB2.
         """
 
@@ -59,10 +58,12 @@ class HashTable:
     def djb2(self, key):
         """
         DJB2 hash, 32-bit
-
         Implement this, and/or FNV-1.
         """
-        # Your code here
+        hash = 5831
+        for c in key:
+            hash = (hash * 33) + ord(c)
+        return hash
 
 
     def hash_index(self, key):
@@ -71,25 +72,52 @@ class HashTable:
         between within the storage capacity of the hash table.
         """
         #return self.fnv1(key) % self.capacity
-        return self.djb2(key) % self.capacity
+        return self.djb2(key) % len(self.capacity)
 
     def put(self, key, value):
         """
         Store the value with the given key.
-
         Hash collisions should be handled with Linked List Chaining.
-
         Implement this.
         """
-        # Your code here
+    
+        index = self.hash_index(key)
+        array = self.capacity
+        ll = array[index]
+        node = HashTableEntry(key,value)
+        
+        print('YYYY', ll)
+        if ll is None:
+            print('here')
+            self.count += 1
+            array.insert(index, node)
+            # ll.head = node
+            return
+            
+        if self.get(key) is False:
+            ('then here')
+            self.count += 1
+            curhead = ll.head
+            ll.head = HashTableEntry(key,value)
+            ll.head.next = curhead
+        
+        else:
+            node = ll.head
+            while node.key != key:
+                node = node.next
+            if node.key == key:
+                node.value = value
+            
+
+        
+
+
 
 
     def delete(self, key):
         """
         Remove the value stored with the given key.
-
         Print a warning if the key is not found.
-
         Implement this.
         """
         # Your code here
@@ -98,56 +126,73 @@ class HashTable:
     def get(self, key):
         """
         Retrieve the value stored with the given key.
-
         Returns None if the key is not found.
-
         Implement this.
         """
-        # Your code here
+        index = self.hash_index(key)
+        ll = self.capacity[index]
+        print('LLL', ll)
+        if ll is not None:
+            print('wtf')
+            node = ll.head
+            while node.key != key:
+                if node.next is None:
+                    return False
+                else:
+                    node = node.next
+                    
+            if node.key == key:
+                return node.value
+        else:
+            return False
 
+       
+
+        
 
     def resize(self, new_capacity):
         """
         Changes the capacity of the hash table and
         rehashes all key/value pairs.
-
         Implement this.
         """
         # Your code here
 
-
+    def printcount(self):
+        print(self.count)
 
 if __name__ == "__main__":
     ht = HashTable(8)
-
-    ht.put("line_1", "'Twas brillig, and the slithy toves")
+    print(ht.capacity)
     ht.put("line_2", "Did gyre and gimble in the wabe:")
-    ht.put("line_3", "All mimsy were the borogoves,")
-    ht.put("line_4", "And the mome raths outgrabe.")
-    ht.put("line_5", '"Beware the Jabberwock, my son!')
-    ht.put("line_6", "The jaws that bite, the claws that catch!")
-    ht.put("line_7", "Beware the Jubjub bird, and shun")
-    ht.put("line_8", 'The frumious Bandersnatch!"')
-    ht.put("line_9", "He took his vorpal sword in hand;")
-    ht.put("line_10", "Long time the manxome foe he sought--")
-    ht.put("line_11", "So rested he by the Tumtum tree")
-    ht.put("line_12", "And stood awhile in thought.")
+    print('blibli',ht.capacity[0].next)
+    ht.put("line_2", "All mimsy were the borogoves,")
+    print('blibli',ht.capacity[0].next.value)
+    # ht.put("line_4", "And the mome raths outgrabe.")
+    # ht.put("line_5", '"Beware the Jabberwock, my son!')
+    # ht.put("line_6", "The jaws that bite, the claws that catch!")
+    # ht.put("line_7", "Beware the Jubjub bird, and shun")
+    # ht.put("line_8", 'The frumious Bandersnatch!"')
+    # ht.put("line_9", "He took his vorpal sword in hand;")
+    # ht.put("line_10", "Long time the manxome foe he sought--")
+    # ht.put("line_11", "So rested he by the Tumtum tree")
+    # ht.put("line_12", "And stood awhile in thought.")
 
-    print("")
+    # print("")
 
-    # Test storing beyond capacity
-    for i in range(1, 13):
-        print(ht.get(f"line_{i}"))
+    # # Test storing beyond capacity
+    # for i in range(1, 13):
+    #     print(ht.get(f"line_{i}"))
 
-    # Test resizing
-    old_capacity = ht.get_num_slots()
-    ht.resize(ht.capacity * 2)
-    new_capacity = ht.get_num_slots()
+    # # Test resizing
+    # old_capacity = ht.get_num_slots()
+    # ht.resize(ht.capacity * 2)
+    # new_capacity = ht.get_num_slots()
 
-    print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+    # print(f"\nResized from {old_capacity} to {new_capacity}.\n")
 
-    # Test if data intact after resizing
-    for i in range(1, 13):
-        print(ht.get(f"line_{i}"))
+    # # Test if data intact after resizing
+    # for i in range(1, 13):
+    #     print(ht.get(f"line_{i}"))
 
-    print("")
+    # print("")
